23674cdba1c9d8da945af76ab5242b9447137031
tujiaw <tujiaw@163.com>
2018-01-30 23:29:15 +0800

    add

--- OptionTrader/App.js
+++ OptionTrader/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, BackHandler } from 'react-native';
+import { StyleSheet, Text, View, BackHandler, ToastAndroid } from 'react-native';
 import { TabNavigator } from 'react-navigation'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Show from './containers/Show'
